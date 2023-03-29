f0095114ef51c11bd0b29f44b21c5fa910421f2a
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-03-29 15:12:09 +0300

    created a appointment  model with its controller

---
+++ db/migrate/20230329121100_create_appointments.rb
@@ -0,0 +1,12 @@
+class CreateAppointments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :appointments do |t|
+      t.integer :pet_id
+      t.integer :user_id
+      t.string :appointment_reason
+      t.integer :start_time
+
+      t.timestamps
+    end
+  end
+end
